79b3ae028ca49445ffcdc5bb691c2cf3da270bd1
Henry Mwoha <104128222+mwigoti@users.noreply.github.com>
2024-05-19 17:08:30 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
+
 If an invalid subreddit is given, the function should return 0
 """
 
